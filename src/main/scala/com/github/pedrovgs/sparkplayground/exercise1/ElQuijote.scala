4f1d168bed1804149cadbd199d2d7a5aa3b64617
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-05-30 00:26:49 +0200

    Update ElQuijote implementation to extend App

--- src/main/scala/com/github/pedrovgs/sparkplayground/exercise1/ElQuijote.scala
+++ src/main/scala/com/github/pedrovgs/sparkplayground/exercise1/ElQuijote.scala
@@ -21,11 +21,13 @@ object ElQuijote extends App with SparkApp {
 
   def numberOfChars(): Double = elQuijoteWords.map(_.length).sum()
 
-  pprint.pprintln("The first sentence in the book is: " + firstSentence())
-  pprint.pprintln("The last sentence in the book is: " + lastSentence())
-  pprint.pprintln("The number of lines the book contains is: " + numberOfLines())
-  pprint.pprintln("The number of words the book contains is: " + numberOfWords())
-  pprint.pprintln("The number of times the word \"Sancho\" appears is: " + numberOfSanchos())
-  pprint.pprintln("The number of chars the book contains is: " + numberOfChars())
-
+  override def main(args: Array[String]): Unit = {
+    super.main(args)
+    pprint.pprintln("The first sentence in the book is: " + firstSentence())
+    pprint.pprintln("The last sentence in the book is: " + lastSentence())
+    pprint.pprintln("The number of lines the book contains is: " + numberOfLines())
+    pprint.pprintln("The number of words the book contains is: " + numberOfWords())
+    pprint.pprintln("The number of times the word \"Sancho\" appears is: " + numberOfSanchos())
+    pprint.pprintln("The number of chars the book contains is: " + numberOfChars())
+  }
 }
