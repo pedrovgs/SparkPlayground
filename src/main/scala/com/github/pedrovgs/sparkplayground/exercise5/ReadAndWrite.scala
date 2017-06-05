80894ffefddc5bdacd0e653e5f19bb6d7d4a3143
pedrovgs <pedrovicente.gomez@gmail.com>
2017-06-05 22:33:10 +0200

    Prepare skeleton to start working with json

---
+++ src/main/scala/com/github/pedrovgs/sparkplayground/exercise5/ReadAndWrite.scala
@@ -0,0 +1,8 @@
+package com.github.pedrovgs.sparkplayground.exercise5
+
+import com.github.pedrovgs.SparkApp
+
+object ReadAndWrite extends App with SparkApp {
+
+
+}
---
+++ src/test/scala/com/github/pedrovgs/sparkplayground/exercise5/ReadAndWriteSpec.scala
@@ -0,0 +1,11 @@
+package com.github.pedrovgs.sparkplayground.exercise5
+
+import com.holdenkarau.spark.testing.SharedSparkContext
+import org.scalatest.{FlatSpec, Matchers}
+
+class ReadAndWriteSpec extends FlatSpec with Matchers with SharedSparkContext {
+
+  "ReadAndWrite" should "read a JSON file, sort by last name and save it again" in {
+
+  }
+}
