eba291447317c34460492947b20281fafe7b213c
pedrovgs <pedrovicente.gomez@gmail.com>
2017-06-06 00:38:12 +0200

    Resolve question related to the json parsing without spark sql

--- src/main/scala/com/github/pedrovgs/Resources.scala
+++ src/main/scala/com/github/pedrovgs/Resources.scala
@@ -2,4 +2,6 @@ package com.github.pedrovgs
 
 trait Resources {
   def getFilePath(name: String): String = getClass.getResource(name).getPath
+
+  def getOutputFilePath(name: String): String = "./outputs/" + name
 }
--- src/main/scala/com/github/pedrovgs/sparkplayground/exercise5/ReadAndWrite.scala
+++ src/main/scala/com/github/pedrovgs/sparkplayground/exercise5/ReadAndWrite.scala
@@ -1,8 +1,45 @@
 package com.github.pedrovgs.sparkplayground.exercise5
 
-import com.github.pedrovgs.SparkApp
+import java.io.File
 
-object ReadAndWrite extends App with SparkApp {
+import com.github.pedrovgs.{Resources, SparkApp}
+import org.apache.commons.io.FileUtils
 
+import scala.util.Try
 
+object ReadAndWrite extends App with SparkApp with Resources {
+
+  def readAndWriteJson(): Unit = {
+    val user = readFirstUserSortedByLastName()
+    writeUserAsJson(user)
+  }
+
+  private def readFirstUserSortedByLastName(): User = {
+    sparkContext
+      .textFile(getFilePath("/exercise5/users.json"))
+      .flatMap(line =>
+        Try {
+          objectMapper.readValue(line, classOf[User])
+        }.toOption)
+      .sortBy(_.name.last)
+      .first()
+  }
+
+  private def writeUserAsJson(user: User): Unit = {
+    val outputFile = getOutputFilePath("/firstUser.json")
+    delete(outputFile)
+    sparkContext
+      .parallelize(List(user))
+      .map(user => {
+        objectMapper.writeValueAsString(user)
+      })
+      .saveAsTextFile(outputFile)
+  }
+
+  private def delete(path: String): Unit = {
+    FileUtils.deleteDirectory(new File(path))
+  }
+
+  pprint.pprintln(
+    "This is the first user if we sort them by their last name: " + readFirstUserSortedByLastName())
 }
