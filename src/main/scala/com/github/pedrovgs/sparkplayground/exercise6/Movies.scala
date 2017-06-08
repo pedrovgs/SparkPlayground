24948b76f5c7f5674540ffa3ed453cbac61437cc
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-06-08 14:39:47 +0200

    Add skeleton

---
+++ src/main/scala/com/github/pedrovgs/sparkplayground/exercise6/Movies.scala
@@ -0,0 +1,7 @@
+package com.github.pedrovgs.sparkplayground.exercise6
+
+import com.github.pedrovgs.SparkApp
+
+object Movies extends App with SparkApp {
+
+}
---
+++ src/test/scala/com/github/pedrovgs/sparkplayground/exercise6/MoviesSpec.scala
@@ -0,0 +1,8 @@
+package com.github.pedrovgs.sparkplayground.exercise6
+
+import com.holdenkarau.spark.testing.SharedSparkContext
+import org.scalatest.{FlatSpec, Matchers}
+
+class MoviesSpec extends FlatSpec with Matchers with SharedSparkContext {
+
+}
