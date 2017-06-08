8d26cf2b45a148f584477d3f54796f1f053e1855
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-06-08 15:36:27 +0200

    Answer the first two questions

--- src/test/scala/com/github/pedrovgs/sparkplayground/exercise6/MoviesSpec.scala
+++ src/test/scala/com/github/pedrovgs/sparkplayground/exercise6/MoviesSpec.scala
@@ -1,8 +1,15 @@
 package com.github.pedrovgs.sparkplayground.exercise6
 
+import com.github.pedrovgs.sparkplayground.exercise6.Movies._
 import com.holdenkarau.spark.testing.SharedSparkContext
 import org.scalatest.{FlatSpec, Matchers}
 
 class MoviesSpec extends FlatSpec with Matchers with SharedSparkContext {
+  "Movies" should "count the number of movies directed by James Camenron" in {
+    numberOfMoviesDirectedByJamesCameron() shouldBe 7
+  }
 
+  it should "count the numer of movies directed by the top five directors" in {
+    numberOfMoviesDirectedByTheTopFiveDirectors() shouldBe 9
+  }
 }
