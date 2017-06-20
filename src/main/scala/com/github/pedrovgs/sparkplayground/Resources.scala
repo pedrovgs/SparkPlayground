a078df9a9378408b2d648e4b6274e4ab5987324e
pedrovgs <pedrovicente.gomez@gmail.com>
2017-06-21 01:09:28 +0200

    Improve how files are read

--- src/main/scala/com/github/pedrovgs/sparkplayground/Resources.scala
+++ src/main/scala/com/github/pedrovgs/sparkplayground/Resources.scala
@@ -6,19 +6,30 @@ import org.apache.spark.SparkFiles
 
 trait Resources {
   def getFilePath(name: String): String = {
-    val jarPath = getClass.getResource(name).getPath
-    val tmpPath = "file:///tmp/resources" + name
+    val jarPath    = getClass.getResource(name).getPath
+    val volumePath = "/tmp/data/resources" + name
     if (exists(jarPath)) {
+      pprint.pprintln("Reading existing jar path at: " + jarPath)
       jarPath
-    } else if (exists(tmpPath)) {
-      tmpPath
+    } else if (exists(volumePath)) {
+      pprint.pprintln("Reading existing volume path at: " + volumePath)
+      "file://" + volumePath
     } else {
-      val fileName = name.substring(name.lastIndexOf("/") + 1)
-      "file://" + SparkFiles.get(fileName)
+      val fileName   = name.substring(name.lastIndexOf("/") + 1)
+      val workerPath = SparkFiles.get(fileName)
+      if (exists(workerPath)) {
+        pprint.pprintln("Reading existing worker path at: " + workerPath)
+      } else {
+        pprint.pprintln("Couln't find file: " + fileName)
+      }
+      "file://" + workerPath
     }
   }
 
   def getOutputFilePath(name: String): String = "./outputs/" + name
 
-  private def exists(path: String): Boolean = Files.exists(Paths.get(path))
+  private def exists(path: String): Boolean = {
+    pprint.pprintln("Checking the existence of a file at path: " + path)
+    Files.exists(Paths.get(path))
+  }
 }
