b82e7f0a942eb2564abb3e79dc960e196766a8b9
pedrovgs <pedrovicente.gomez@gmail.com>
2017-07-04 01:34:41 +0200

    I got it working thanks to the MLUtils lib

--- src/main/scala/com/github/pedrovgs/sparkplayground/exercise11/MachineLearning.scala
+++ src/main/scala/com/github/pedrovgs/sparkplayground/exercise11/MachineLearning.scala
@@ -3,11 +3,12 @@ package com.github.pedrovgs.sparkplayground.exercise11
 import com.github.pedrovgs.sparkplayground.SparkApp
 import com.github.pedrovgs.sparkplayground.exercise5.ReadAndWrite.getFilePath
 import org.apache.spark.ml.feature.Word2Vec
-import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.classification.SVMWithSGD
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.mllib.linalg.DenseVector
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.rdd.RDD
 
 object MachineLearning extends SparkApp {
 
@@ -37,13 +38,15 @@ object MachineLearning extends SparkApp {
     .transform(tweetsData)
 
   // Transform as labeled points
-  val data: RDD[LabeledPoint] = result.rdd.map(row => {
+  val convertedResult = MLUtils.convertVectorColumnsFromML(result, "features")
+  val data: RDD[LabeledPoint] = convertedResult.rdd.map(row => {
     val label = row.getAs[Double]("label")
-    val features = Vectors.dense(row.getAs[Array[Double]]("features"))
+    val features = row.getAs[DenseVector]("features")
     new LabeledPoint(label, features)
   })
 
   // Split data into training (60%) and test (40%).
+
   val splits: Array[RDD[LabeledPoint]] = data.randomSplit(Array(0.6, 0.4), seed = 11L)
   val training: RDD[LabeledPoint] = splits(0).cache()
   val test: RDD[LabeledPoint] = splits(1)
