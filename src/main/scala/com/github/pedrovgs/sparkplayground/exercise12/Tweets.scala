beeeb6c2ce9d339b9521b81198c1bd21e40e27f7
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-08-05 15:30:54 +0200

    Create skeleton needed to resolve our exercise

---
+++ src/main/scala/com/github/pedrovgs/sparkplayground/exercise12/Tweets.scala
@@ -0,0 +1,7 @@
+package com.github.pedrovgs.sparkplayground.exercise12
+
+import com.github.pedrovgs.sparkplayground.{Resources, SparkApp}
+
+class Tweets extends SparkApp with Resources {
+
+}
---
+++ src/test/scala/com/github/pedrovgs/sparkplayground/exercise12/TweetsSpec.scala
@@ -0,0 +1,11 @@
+package com.github.pedrovgs.sparkplayground.exercise12
+
+import com.holdenkarau.spark.testing.SharedSparkContext
+import org.scalatest.{FlatSpec, Matchers}
+
+class TweetsSpec extends FlatSpec with Matchers with SharedSparkContext {
+
+  "Tweets" should "test" in {
+    true shouldBe true
+  }
+}
